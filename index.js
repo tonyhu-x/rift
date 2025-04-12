39f6c319013055e2deb4a13bbcae9cf287d1a3e8
Tony Juntao Hu <54758881+tonyhu-x@users.noreply.github.com>
2025-04-12 15:32:06 -0400

    Create "roles"

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const COLOURS = {
   adc: "#bdd5ef",
   support: "#e3c4d9",
   // This is an even mix of all the colours above
-  default: "#dfd0cd",
+  any: "#dfd0cd",
 };
 
 /**
@@ -14,6 +14,21 @@ const COLOURS = {
  */
 function createLabel(name, username) {
   return `<b>${name}</b>\na.k.a.\n${username}`;
+}
+
+function assignColour(profile) {
+  let topRole;
+  if (Array.isArray(profile.roles)) {
+    topRole = profile.roles[0];
+  } else {
+    // Should be "any" here
+    topRole = profile.roles;
+  }
+
+  return {
+    background: COLOURS[topRole],
+    border: COLOURS[topRole],
+  };
 }
 
 const options = {
@@ -39,17 +54,14 @@ let [profiles, relations] = await Promise.all([
   (async () => (await fetch("relations.json")).json())(),
 ]);
 
+profiles = profiles.map((profile) => ({
+  ...profile,
+  label: createLabel(profile.name, profile.username),
+  color: assignColour(profile),
+}));
+
 let nodes = new vis.DataSet(profiles);
 const edges = new vis.DataSet(relations);
-
-nodes = nodes.map((node) => ({
-  ...node,
-  label: createLabel(node.name, node.username),
-  color: {
-    background: COLOURS[node.role ?? "default"],
-    border: COLOURS[node.role ?? "default"],
-  },
-}));
 
 // Create a network
 const container = document.getElementById("diagram");
